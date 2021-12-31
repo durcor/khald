55871624917009d883e36c3e21989f861d94ece0
Tyler Kaminski <durcor@disroot.org>
2021-12-31 02:04:24 -0500

    Fix config directory backup

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ mkdir -p "${XDG_CONFIG_HOME:-$HOME/.config}/khald"
 cp config "${XDG_CONFIG_HOME:-$HOME/.config}/khald/"
 cp khald "$HOME/.local/bin/"
 cp khal-import.desktop "$HOME/.local/share/applications/"
-cp khald.service "${XDG_CONFIG_HOME-$HOME/.config}/systemd/user/"
+cp khald.service "${XDG_CONFIG_HOME:-$HOME/.config}/systemd/user/"
 systemctl --user enable --now khald
